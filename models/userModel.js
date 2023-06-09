9ae0374efdf9e0f5f8cc16ac12a38264540af28b
leventsamlioglu <leventsamlioglu@icloud.com>
2023-06-09 08:52:00 +0200

    signup page activated

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
 	username: {
@@ -15,7 +16,13 @@ const userSchema = new mongoose.Schema({
 		minlength: [1, "Password length must be at least 6 character!"],
 	},
 });
-  
+
+// Hash password before save the user to DB
+userSchema.pre("save", async function (next) {
+	this.password = bcrypt.hashSync(this.password, 12);
+	next();
+});
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
